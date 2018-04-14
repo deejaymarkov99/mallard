e412a3c30e4ef03e8f492cde17dcaf477cecc251
Damjan Markovic <greywhiteblue1972@gmail.com>
2018-04-14 12:32:41 -0400

    Added Morgan

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-// eslint-ignore no-console
+// eslint-ignore no-consolee
+const Morgan = require('morgan');
 const Express = require('express');
 
 const app = new Express();
+
+app.use(Morgan("tiny"));
 
 app.use(Express.static('dist'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
